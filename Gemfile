bac379eac4f3a9551fb019e302bf3159e121188a
Rocio Jalifi <jalifirocio@gmail.com>
2021-06-24 10:22:02 +0200

    adds nav-bar

--- Gemfile
+++ Gemfile
@@ -37,8 +37,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'bootstrap', '~> 4.6'
 end
-
+gem 'font-awesome-sass'
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying Flats'
+Flat.destroy_all
+puts 'Creating new flats'
+
+Flat.create!(
+  name:'Hermosa casa de campo en Amsterdam',
+  address: 'Bijlmerdreef 900, 1100th Amsterdam',
+  description: 'Hermosa cassa de campo muy espaciosa',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Cozy departamento',
+  address: 'Bijlmerdreef 800, 1100th Utrech',
+  description: 'Cozy departamento en el centro de Utretch',
+  price_per_night: 100,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Romantic Suite',
+  address: 'Buenos Aires 800, 1140tw La Haya',
+  description: 'Suite romantica para disfrutar con tu ser amado',
+  price_per_night: 200,
+  number_of_guests: 2 
+)
